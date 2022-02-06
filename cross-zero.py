92732fb5bb7da44c0a0eb5f4a4eeb12ae5390e23
Marina F <fedma001@mail.ru>
2022-02-06 19:36:26 +0300

    field and first turn made

--- cross-zero.py
+++ cross-zero.py
@@ -6,6 +6,7 @@ def motion(field):
     str_1 = input("Введите координаты Вашего хода в формате: строка,столбец ")
     x1=int(str_1[0])
     y1=int(str_1[2])
-    np.put(field, x1-1, 1)
+    # np.put(field, [x1-1, y1-1], 1)
+    field[x1-1][y1-1] = 1
     return(field)
 print(motion(field))
