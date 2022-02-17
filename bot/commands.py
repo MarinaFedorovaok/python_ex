43d8aab8dcd441cec89b8af72ef180a5f02c8083
Marina F <fedma001@mail.ru>
2022-02-17 13:00:58 +0300

    added new command

--- bot/commands.py
+++ bot/commands.py
@@ -1,5 +1,7 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
+import numpy as np
+import matplotlib.pyplot as plt
 
 def hello(update: Update, context: CallbackContext):
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
@@ -13,3 +15,20 @@ def sum(update: Update, context: CallbackContext):
     x = int(items[1])
     y = int(items[2])
     update.message.reply_text(f'{x} + {y} = {x+y}')
+
+def pict(update: Update, context: CallbackContext):
+    msg = update.message.text
+    items = msg.split()# сплитим сообщение на /pict 1 2 3
+    a = int(items[1])
+    b = int(items[2])
+    c = int(items[3])
+    x = np.arange(a, b, c)
+    y = np.sin(x)
+    plt.plot(x, y)
+    plt.savefig('saved_figure.png') #сохраняем график в файл
+    #update.message.reply_text(f'sin {a}, {b}, {c}')
+    photo_file = open("saved_figure.png",'rb')
+    context.bot.sendPhoto(chat_id=update.message.chat_id,
+                            photo=photo_file,
+                            caption="sin {a}, {b}, {c}")
+ 
--- bot/my_lesson_bot.py
+++ bot/my_lesson_bot.py
@@ -1,5 +1,7 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
+import numpy as np
+import matplotlib.pyplot as plt
 import commands as c
 
 
@@ -9,6 +11,7 @@ print("Server starts")
 updater.dispatcher.add_handler(CommandHandler('hello', c.hello))
 updater.dispatcher.add_handler(CommandHandler('help', c.help))
 updater.dispatcher.add_handler(CommandHandler('sum', c.sum))
+updater.dispatcher.add_handler(CommandHandler('pict', c.pict))
 
 
 updater.start_polling()
